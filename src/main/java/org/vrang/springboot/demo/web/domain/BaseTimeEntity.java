9eed978773eb2f0bd0c364d84a775a5674f67417
mingi <tlsalsrl0617@gmail.com>
2021-01-23 19:01:20 +0900

    Create: BaseTimeEntity.java
    상속받을 경우 엔티티 생성, 수정 시간을 얻을 수 있다

---
+++ src/main/java/org/vrang/springboot/demo/web/domain/BaseTimeEntity.java
@@ -0,0 +1,22 @@
+package org.vrang.springboot.demo.web.domain;
+
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTimeEntity
+{
+    @CreatedDate
+    private LocalDateTime createDateTime;
+    
+    @LastModifiedDate
+    private LocalDateTime modifiedDateTime;
+}
