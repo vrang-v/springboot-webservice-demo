e6c403b06d179588147d09e701e7e803670d1689
mingi <tlsalsrl0617@gmail.com>
2021-01-28 00:31:52 +0900

    Chore: deploy.sh 오타수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.properties,classpath:/application-real.yaml,/home/ec2-user/app/application-oauth.yaml,/home/ec2-user/app/application-real-db.yaml \
+  -Dspring.config.location=classpath:/application.yaml,classpath:/application-real.yaml,/home/ec2-user/app/application-oauth.yaml,/home/ec2-user/app/application-real-db.yaml \
   -Dspring.profiles.active=real \
   $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
